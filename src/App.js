aeacc50214e94c288800c481faa2aaafa643949a
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-07 15:05:06 -0300

    label, input component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Banner from './components/Banner/Banner';
-
+import Banner from './components/Banner/Banner.js';
+import CampoTexto from './components/CampoTexto/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CampoTexto label="Nome" placeholder="Digite o seu nome" />
+      <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
+      <CampoTexto label="Imagem" placeholder="Insira uma imagem" />
     </div>
   );
 }
