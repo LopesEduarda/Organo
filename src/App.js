15a7b3cbaf840beac5610a99e2e5321acc5045d1
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-07 15:27:35 -0300

    form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './components/Banner/Banner.js';
-import CampoTexto from './components/CampoTexto/index.js';
+import Formulario from './components/Formulario/index.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-      <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Insira uma imagem" />
+      <Formulario />
     </div>
   );
 }
