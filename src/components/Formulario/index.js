a3fd17773426aedf78e93a3e6c2752bc996425a8
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-08 13:39:24 -0300

    map

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,13 @@
 import './Formulario.css';
 import CampoTexto from '../../components/CampoTexto/index.js';
+import ListaSuspense from '../ListaSuspensa/index.js';
 
 const Formulario = () => {
+
+    const times = [
+        'Programação', 'Front-End', 'Data Science', 'Devops', 'UX e Design', 'Mobile', 'Inovação e Gestão'
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -9,6 +15,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite o seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Insira uma imagem" />
+                <ListaSuspense itens={times}/>
             </form>
         </section>
     )
