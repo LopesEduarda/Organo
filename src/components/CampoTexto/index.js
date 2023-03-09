08bfd1d1ad57d3ab57300ffea044e04f55050796
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-09 15:58:58 -0300

    finish state on form

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,10 +1,26 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value);
+    }
+    // o value é atrelado ao estado: se o value muda, o estado muda. e vice-versa
+    // capturando o valor do input, cada letra que for digitada(incluindo os espaços) aparecerão no event.target.value.
+    /*
+        Para entender melhor todo o ciclo de vida e o poder do React é importante entender os disparos eo evento,
+        e também como gerenciamos o estado das nossas variáveis e como o estado da nossa variável interna associada aos componentes visuais
+    */
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input
+                value={props.value}
+                onChange={aoDigitado}
+                required={props.obrigatorio}
+                placeholder={props.placeholder}
+            />
         </div>
     )
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,28 +1,62 @@
+import { useState } from 'react';
 import './Formulario.css';
 import CampoTexto from '../../components/CampoTexto/index.js';
 import ListaSuspensa from '../ListaSuspensa/index.js';
 import Botao from '../Botao';
 
+
 const Formulario = () => {
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('');
+    // vai retornar o valor em si + o set, que é pra quando eu quiser alterar, essa função set será chamada
+    // não posso alterar o valor direto por atribuição, é preciso passar pelo set
+
+    // hook: é algo que o React entrega pra gente para gente conseguir manter um estado dentro de uma função,
+    // porque originalmente uma função não tem estado, porque ela é uma função
 
     const times = [
         'Programação', 'Front-End', 'Data Science', 'Devops', 'UX e Design', 'Mobile', 'Inovação e Gestão'
     ]
 
-    const aoSalvar = (event) => {
-        event.preventDefault();
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
         // previne o comportamento padrão de recarregar a página
-        console.log('Form foi submetido')
+        console.log('Form foi submetido  =>', nome, cargo, imagem, time)
     }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Insira uma imagem" />
-                <ListaSuspensa obrigatorio={true} label="Times" itens={times} />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite o seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite o seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Insira uma imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Times"
+                    itens={times} 
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                    />
                 <Botao>
                     Criar Card
                 </Botao>
