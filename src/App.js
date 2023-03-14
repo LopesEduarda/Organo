1698ef1a091b96bc8854aff8e3d768d858fa8342
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-14 15:17:38 -0300

    add info about props and states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import Banner from './components/Banner/Banner.js';
 import Formulario from './components/Formulario/index.js';
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+  // estado + sua função de setter para mudar o estado, porque não é possivel mudá-lo diretamente,
+  // é necessário passar pela função de setter
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+    // função de setter para fazer uma cópia do array antigo de colaboradores, passando o novo colaborador
+  }
+  
+  // função para exibir o novo colaborador adicionado, que recebe como parâmetro um colaborador
+  // na linha 18, estamos passando essa função para dentro do Formulário, para conseguir acessar lá dentro através das props
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario 
+        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
+      />
     </div>
   );
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,73 +1,83 @@
-import { useState } from 'react';
-import './Formulario.css';
-import CampoTexto from '../../components/CampoTexto/index.js';
-import ListaSuspensa from '../ListaSuspensa/index.js';
-import Botao from '../Botao';
+import { useState } from "react";
+import "./Formulario.css";
+import CampoTexto from "../../components/CampoTexto/index.js";
+import ListaSuspensa from "../ListaSuspensa/index.js";
+import Botao from "../Botao";
 
+const Formulario = (props) => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+  // vai retornar o valor em si + o set, que é pra quando eu quiser alterar, essa função set será chamada
+  // não posso alterar o valor direto por atribuição, é preciso passar pelo set
 
-const Formulario = () => {
-    const [nome, setNome] = useState('');
-    const [cargo, setCargo] = useState('');
-    const [imagem, setImagem] = useState('');
-    const [time, setTime] = useState('');
-    // vai retornar o valor em si + o set, que é pra quando eu quiser alterar, essa função set será chamada
-    // não posso alterar o valor direto por atribuição, é preciso passar pelo set
+  // hook: é algo que o React entrega pra gente para gente conseguir manter um estado dentro de uma função,
+  // porque originalmente uma função não tem estado, porque ela é uma função
 
-    // hook: é algo que o React entrega pra gente para gente conseguir manter um estado dentro de uma função,
-    // porque originalmente uma função não tem estado, porque ela é uma função
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
 
-    const times = [
-        'Programação', 'Front-End', 'Data Science', 'Devops', 'UX e Design', 'Mobile', 'Inovação e Gestão'
-    ]
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    // previne o comportamento padrão de recarregar a página
 
-    const aoSalvar = (evento) => {
-        evento.preventDefault();
-        // previne o comportamento padrão de recarregar a página
-        console.log('Form foi submetido  =>', nome, cargo, imagem, time)
-    }
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
+    // essa props.aoColaboradorCadastrado está no App.js e estamos recebendo aqui como props!
+  };
 
-    return (
-        <section className="formulario">
-            <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Nome"
-                    placeholder="Digite o seu nome"
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                    // o aoAlterado recebe a função Set para mudar
-                    // isso poderia ser extraído para uma função, mas aqui a gente já tem o acesso ao valor
-                    // estamos somente setando o estado
-                    // aqui estamos fazendo um Set do nome, com o valor que recebemos no estado
-                    // ao alterado poderia ser onChange
-                />
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Cargo"
-                    placeholder="Digite o seu cargo"
-                    valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}
-                />
-                <CampoTexto
-                    label="Imagem"
-                    placeholder="Insira uma imagem"
-                    valor={imagem}
-                    aoAlterado={valor => setImagem(valor)}
-                />
-                <ListaSuspensa
-                    obrigatorio={true}
-                    label="Times"
-                    itens={times} 
-                    valor={time}
-                    aoAlterado={valor => setTime(valor)}
-                    />
-                <Botao>
-                    Criar Card
-                </Botao>
-            </form>
-        </section>
-    )
-}
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite o seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+          // o aoAlterado recebe a função Set para mudar
+          // isso poderia ser extraído para uma função, mas aqui a gente já tem o acesso ao valor
+          // estamos somente setando o estado
+          // aqui estamos fazendo um Set do nome, com o valor que recebemos no estado
+          // ao alterado poderia ser onChange
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite o seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Insira uma imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Times"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
+        <Botao>Criar Card</Botao>
+      </form>
+    </section>
+  );
+};
 
 export default Formulario;
