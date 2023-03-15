3ee638900dda1fbe80d3d3f38c37a051ed93c6ce
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-03-15 16:37:21 -0300

    component times finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import { useState } from 'react';
-import Banner from './components/Banner/Banner.js';
-import Formulario from './components/Formulario/index.js';
-import Time from './components/Time/index.js';
+import { useState } from "react";
+import Banner from "./components/Banner/Banner.js";
+import Formulario from "./components/Formulario/index.js";
+import Time from "./components/Time/index.js";
 
 function App() {
+  const times = [
+    {
+      nome: "Programação",
+      corPrimaria: "#57C278",
+      corSecundaria: "#D9F7E9",
+    },
+    {
+      nome: "Front-End",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
+    },
+    {
+      nome: "Data Science",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#F0F8E2",
+    },
+    {
+      nome: "Devops",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
+    },
+    {
+      nome: "UX e Design",
+      corPrimaria: "#DB6EBF",
+      corSecundaria: "#FAE9F5",
+    },
+    {
+      nome: "Mobile",
+      corPrimaria: "#FFBA05",
+      corSecundaria: "#FFF5D9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8A29",
+      corSecundaria: "#FFEEDF",
+    },
+  ];
+  // times deixou de ser um array e virou um array de objetos, porque cada time tem sua cor primária e secundária
+
   const [colaboradores, setColaboradores] = useState([]);
   // estado + sua função de setter para mudar o estado, porque não é possivel mudá-lo diretamente,
   // é necessário passar pela função de setter
@@ -12,19 +51,29 @@ function App() {
     console.log(colaborador);
     setColaboradores([...colaboradores, colaborador]);
     // função de setter para fazer uma cópia do array antigo de colaboradores, passando o novo colaborador
-  }
-  
+  };
+
   // função para exibir o novo colaborador adicionado, que recebe como parâmetro um colaborador
   // na linha 18, estamos passando essa função para dentro do Formulário, para conseguir acessar lá dentro através das props
+ 
+  // a chave key é usada para controlar a renderização
   return (
     <div className="App">
       <Banner />
-      <Formulario 
-        aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
       />
-      <Time nome="Programação" />
-      <Time nome="Front-end" />
-      <Time nome="Data-Science" />
+
+      {times.map((time) => (
+        <Time 
+        key={time.nome} 
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -2,8 +2,10 @@ import "./Time.css";
 
 const Time = (props) => {
   return (
-    <section className="time">
-      <h3>{props.nome}</h3>
+    // dentro das chaves do style={} abrimos outra chave {} para indicar que há uma variável ali dentro
+    // assim, colocamos as estilizações que, nesse caso, está no App.js
+    <section className="time" style={ {backgroundColor: props.corSecundaria} }>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
     </section>
   );
 };
