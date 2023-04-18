0158213f390f10dc265f3a188f5f031dbe8e69d1
Maria Eduarda <eduardaloopes2011@hotmail.com>
2023-04-18 15:22:20 -0300

    input color

--- src/App.js
+++ src/App.js
@@ -2,45 +2,53 @@ import { useState } from "react";
 import Banner from "./components/Banner/Banner.js";
 import Formulario from "./components/Formulario/index.js";
 import Time from "./components/Time/index.js";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const times = [
+  const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: "Programação",
       corPrimaria: "#57C278",
       corSecundaria: "#D9F7E9",
     },
     {
+      id: uuidv4(),
       nome: "Front-End",
       corPrimaria: "#82CFFA",
       corSecundaria: "#E8F8FF",
     },
     {
+      id: uuidv4(),
       nome: "Data Science",
       corPrimaria: "#A6D157",
       corSecundaria: "#F0F8E2",
     },
     {
+      id: uuidv4(),
       nome: "Devops",
       corPrimaria: "#E06B69",
       corSecundaria: "#FDE7E8",
     },
     {
+      id: uuidv4(),
       nome: "UX e Design",
       corPrimaria: "#DB6EBF",
       corSecundaria: "#FAE9F5",
     },
     {
+      id: uuidv4(),
       nome: "Mobile",
       corPrimaria: "#FFBA05",
       corSecundaria: "#FFF5D9",
     },
     {
+      id: uuidv4(),
       nome: "Inovação e Gestão",
       corPrimaria: "#FF8A29",
       corSecundaria: "#FFEEDF",
     },
-  ];
+  ]);
   // times deixou de ser um array e virou um array de objetos, porque cada time tem sua cor primária e secundária
 
   const [colaboradores, setColaboradores] = useState([]);
@@ -49,6 +57,24 @@ function App() {
 
   function deletarColaborador() {
     console.log('função deletar colaborador');
+  }
+
+  // function mudarcorDoTime(cor, id) {
+  //   setTimes(times.map(time => {
+  //     if(time.id === id) {
+  //       time.corSecundaria = cor;
+  //     }
+  //     return time;
+  //   }));
+  // }
+
+  function mudarcorDoTime(corPrimaria, corSecundaria, id) {
+    setTimes(times.map(time => {
+      if(time.id === id) {
+        return { ...time, corPrimaria, corSecundaria };
+      }
+      return time;
+    }));
   }
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -73,7 +99,9 @@ function App() {
 
       {times.map((time) => (
         <Time
-          key={time.nome}
+          mudarCor={mudarcorDoTime}
+          key={time.id}
+          id={time.id}
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -3,9 +3,8 @@ import Colaborador from "../Colaborador";
 
 const Time = (props) => {
   return (
-    // dentro das chaves do style={} abrimos outra chave {} para indicar que há uma variável ali dentro
-    // assim, colocamos as estilizações que, nesse caso, está no App.js
     <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+      <input value={props.corPrimaria} onChange={(evento) => props.mudarCor(evento.target.value, props.corSecundaria, props.id)} type='color' className="input-cor" />
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="colaboradores">
         {props.colaboradores.map((colaborador) => {
@@ -24,3 +23,4 @@ const Time = (props) => {
 };
 
 export default Time;
+
